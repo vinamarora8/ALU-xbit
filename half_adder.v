4b35f903d95ab5e7b24890a905ce887152b2fdbe
vinamarora8 <vinam.arora8@gmail.com>
2016-12-02 17:55:52 +0530

    Initial Commit

---
+++ half_adder.v
@@ -0,0 +1,25 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    17:52:37 12/02/2016 
+// Design Name: 
+// Module Name:    half_adder 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module half_adder(
+    );
+
+
+endmodule
