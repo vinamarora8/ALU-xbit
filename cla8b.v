7df90f21b99a00025e90de1f0bba92a954813d95
vinamarora8 <vinam.arora8@gmail.com>
2016-12-05 17:55:00 +0530

    Fixes a minor bug + -> |

--- cla8b.v
+++ cla8b.v
@@ -43,13 +43,13 @@ module cla8b(inA, inB, cin, sum, cout
 	full_adder_reduced fa8(inA[7], inB[7], c7, sum[7], p7, g7);
 	
 	//// Carry Generation Logic
-	assign c1 	= g0 + (p0*cin);
-	assign c2 	= g1 + (p1*g0) + (p1*p0*cin);
-	assign c3 	= g2 + (p2*g1) + (p2*p1*g0) + (p2*p1*p0*cin);
-	assign c4 	= g3 + (p3*g2) + (p3*p2*g1) + (p3*p2*p1*g0) + (p3*p2*p1*p0*cin);
-	assign c5 	= g4 + (p4*g3) + (p4*p3*g2) + (p4*p3*p2*g1) + (p4*p3*p2*p1*g0) + (p4*p3*p2*p1*p0*cin);
-	assign c6 	= g5 + (p5*g4) + (p5*p4*g3) + (p5*p4*p3*g2) + (p5*p4*p3*p2*g1) + (p5*p4*p3*p2*p1*g0) + (p5*p4*p3*p2*p1*p0*cin);
-	assign c7 	= g6 + (p6*g5) + (p6*p5*g4) + (p6*p5*p4*g3) + (p6*p5*p4*p3*g2) + (p6*p5*p4*p3*p2*g1) + (p6*p5*p4*p3*p2*p1*g0) + (p6*p5*p4*p3*p2*p1*p0*cin);
-	assign cout = g7 + (p7*g6) + (p7*p6*g5) + (p7*p6*p5*g4) + (p7*p6*p5*p4*g3) + (p7*p6*p5*p4*p3*g2) + (p7*p6*p5*p4*p3*p2*g1) + (p7*p6*p5*p4*p3*p2*p1*g0) + (p7*p6*p5*p4*p3*p2*p1*p0*cin);
+	assign c1 	= g0 | (p0*cin);
+	assign c2 	= g1 | (p1*g0) | (p1*p0*cin);
+	assign c3 	= g2 | (p2*g1) | (p2*p1*g0) | (p2*p1*p0*cin);
+	assign c4 	= g3 | (p3*g2) | (p3*p2*g1) | (p3*p2*p1*g0) | (p3*p2*p1*p0*cin);
+	assign c5 	= g4 | (p4*g3) | (p4*p3*g2) | (p4*p3*p2*g1) | (p4*p3*p2*p1*g0) | (p4*p3*p2*p1*p0*cin);
+	assign c6 	= g5 | (p5*g4) | (p5*p4*g3) | (p5*p4*p3*g2) | (p5*p4*p3*p2*g1) | (p5*p4*p3*p2*p1*g0) | (p5*p4*p3*p2*p1*p0*cin);
+	assign c7 	= g6 | (p6*g5) | (p6*p5*g4) | (p6*p5*p4*g3) | (p6*p5*p4*p3*g2) | (p6*p5*p4*p3*p2*g1) | (p6*p5*p4*p3*p2*p1*g0) | (p6*p5*p4*p3*p2*p1*p0*cin);
+	assign cout = g7 | (p7*g6) | (p7*p6*g5) | (p7*p6*p5*g4) | (p7*p6*p5*p4*g3) | (p7*p6*p5*p4*p3*g2) | (p7*p6*p5*p4*p3*p2*g1) | (p7*p6*p5*p4*p3*p2*p1*g0) | (p7*p6*p5*p4*p3*p2*p1*p0*cin);
 
 endmodule
